ae77a0db9b652a8f9eeaa204efd148c561df89e4
António Ribeiro <am.ribeiro@campus.fct.unl.pt>
2014-11-25 03:16:14 +0000

    Everything works by now. Os pontos já estão sempre entre o -1 e o 1. Daí conseguir-se ajustar sozinho.

--- src/BuildObject.java
+++ src/BuildObject.java
@@ -12,8 +12,12 @@ import javax.media.opengl.glu.GLU;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
+import com.jogamp.opengl.util.gl2.GLUT;
+
 import static javax.media.opengl.GL.*;  // GL constants
 import static javax.media.opengl.GL2.*; // GL2 constants
+import static javax.media.opengl.fixedfunc.GLMatrixFunc.GL_MODELVIEW;
+import static javax.media.opengl.fixedfunc.GLMatrixFunc.GL_PROJECTION;
 
 /**
  * @author M. Próspero (Updated to JOGL2 by Fernando Birra)
@@ -25,85 +29,91 @@ public class BuildObject implements GLEventListener {
 	public static int DEFAULT_WIDTH = 400;
 	public static int DEFAULT_HEIGHT = 280;
 	
-	private ObjectLoader obj;
+	private Shape obj;
 	private int width = DEFAULT_WIDTH, height = DEFAULT_HEIGHT;
 	
 	@Override
 	public void display(GLAutoDrawable drawable) {
 		GL2 gl = drawable.getGL().getGL2();
-		GLU glu = new GLU();
-		
-		gl.glClearColor(0,0,0,0);
-		gl.glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
-
-		gl.glMatrixMode(GL_PROJECTION);  // Set up the projection.
+		GLUT glut = new GLUT() ;
+		double aRatio = width / (double)height; 
+	    	    
+	    gl.glMatrixMode(GL_PROJECTION);
 		gl.glLoadIdentity();
-		gl.glOrtho(-10, 10, -10, 10, -20, 20);
+		
+		gl.glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
+		gl.glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		
+	    if (width <= height)
+	    	gl.glOrtho (-1.5, 1.5, -1.5/aRatio, 1.5/aRatio, -1.5, 1.5);
+	    else
+	    	gl.glOrtho (-1.5*aRatio, 1.5*aRatio, -1.5, 1.5, -1.5, 1.5);
+	    
 		gl.glMatrixMode(GL_MODELVIEW);
-			
+		gl.glLoadIdentity();
+	    
 		gl.glViewport(0, (height/2), (width/2), (height/2));
-		drawObj(gl);
+		displayScene(gl);
 				
 		gl.glViewport((width/2), (height/2), (width/2), (height/2));
 		gl.glLoadIdentity();
-		gl.glPushMatrix();  
 		gl.glRotatef(90, 0, 1, 0);
-		drawObj(gl);
-		gl.glPopMatrix();
+		displayScene(gl);
 		
 		gl.glViewport(0, 0, (width/2), (height/2));
 		gl.glLoadIdentity();
-		gl.glPushMatrix();  
 		gl.glRotatef(90, 1, 0, 0);
-		drawObj(gl);
-		gl.glPopMatrix();
+		displayScene(gl);
 		
 		gl.glViewport((width/2), 0, (width/2), (height/2));
 		gl.glLoadIdentity();
-		gl.glPushMatrix();  
 		gl.glRotatef(10, 1, 0, 0);
 		gl.glRotatef(235, 0, 1, 0);
-		gl.glTranslatef(5, 2.5f, 0);
-		drawObj(gl);
-		gl.glPopMatrix();
+		displayScene(gl);
+	    
+	    gl.glFlush() ;
 	}
 	
 	private void drawObj(GL2 gl){
 		for(Face f : obj.getFaces()){
-			//gl.glColor3d(Math.random(), Math.random(), Math.random());         // Color 
 			gl.glBegin(GL_POLYGON);
 			for(Point3D p : f.getPoints()){
-				gl.glVertex3f(p.getX(), p.getY(), p.getZ());
-				System.out.println("X: "+p.getX()+" Y: "+p.getY()+" Z: "+p.getZ());
+				gl.glVertex3f(p.getX()/obj.getMaxAbs(), p.getY()/obj.getMaxAbs(), p.getZ()/obj.getMaxAbs());
+				System.out.println("X: "+p.getX()/obj.getMaxAbs()+" Y: "+p.getY()/obj.getMaxAbs()+" Z: "+p.getZ()/obj.getMaxAbs());
 			}
 			gl.glEnd();
 		}
 	}
+	
+	public void displayScene(GL2 gl) {
+		
+		GLUT glu = new GLUT() ;
+		//gl.glMatrixMode(GL_MODELVIEW);
+		//gl.glLoadIdentity();
+		gl.glColor3d(1.0, 0.0, 0.0);
+		
+		//gl.glRotated(30.0, 1.0, 0, 0);
+		//gl.glRotated(30.0, 0, 1, 0);
+
+		drawObj(gl);
+	}
 
 	
 	@Override
-	public void dispose(GLAutoDrawable arg0) {
-		// TODO Auto-generated method stub
-		
-	}
+	public void dispose(GLAutoDrawable arg0) { }
 
 	@Override
 	public void init(GLAutoDrawable drawable) {
 		GL2 gl = drawable.getGL().getGL2();
-		gl.glClearColor(0.2f, 0.2f, 0.2f, 0.2f);
+		gl.glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
 		gl.glEnable(GL_DEPTH_TEST);
-		try {
-			obj = new ObjectLoader();
-			obj.load(new File("objects/atom.obj"));
-		} catch (IOException e) {e.printStackTrace();}
-		gl.glPolygonMode(GL_FRONT, GL_LINE);
-		gl.glPolygonMode(GL_BACK, GL_LINE);
+		obj = new Shape("objects/berserker.obj");
+		gl.glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 	}
 
 	@Override
 	public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
-		this.width = width;
-		this.height = height;
+		this.width = width; this.height = height;
 	}
 
 	public static void main(String[] args) {
---
+++ src/Shape.java
@@ -0,0 +1,163 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Vector;
+
+public class Shape {
+
+	private final static String OBJ_VERTEX = "v";
+	private final static String OBJ_VERTEX_TEXTURE = "vt";
+	private final static String OBJ_FACE = "f";
+	
+	private final static String DEFAULT_OBJ_PATH = "objects/cube.obj";
+
+	private Vector<Point3D> points = new Vector<Point3D>();
+	private Vector<Face> faces = new Vector<Face>();
+	
+	private float xMin = Float.MAX_VALUE, xMax = Float.MIN_VALUE, 
+					yMin = Float.MAX_VALUE, yMax = Float.MIN_VALUE, 
+					 zMin = Float.MAX_VALUE, zMax = Float.MIN_VALUE;
+	
+	private float maxAbs = Float.MIN_VALUE; 
+	
+	private int nFaces = 0, nEdges = 0;  
+
+	/*** CONTRUCTORS ***/
+	
+	public Shape(){
+		try {
+			load(new File(DEFAULT_OBJ_PATH));
+		} catch (IOException e) { e.printStackTrace(); }
+	}
+	
+	public Shape(String path){
+		try {
+			load(new File(path));
+		} catch (IOException e) { e.printStackTrace(); }
+	}
+	
+	/*** GETTERS ***/
+
+	public Vector<Point3D> getPoints() {
+		return points;
+	}
+
+	public Vector<Face> getFaces() {
+		return faces;
+	}
+	
+	public float getxMin() {
+		return xMin;
+	}
+
+	public float getxMax() {
+		return xMax;
+	}
+
+	public float getyMin() {
+		return yMin;
+	}
+
+	public float getyMax() {
+		return yMax;
+	}
+
+	public float getzMin() {
+		return zMin;
+	}
+
+	public float getzMax() {
+		return zMax;
+	}
+	
+	public float getMaxAbs() {
+		return maxAbs;
+	}
+
+	/*** SETTERS ***/
+
+
+	/*** LOAD ***/
+
+	public void load(File objFile) throws IOException {
+
+		FileReader fileReader = new FileReader(objFile);
+		BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+		int lineCount = 0;
+		int nFaces = 0;
+
+		String line = null;
+		while (true) {
+			line = bufferedReader.readLine();
+			if (null == line) {
+				break;
+			}
+
+			line = line.trim();
+
+			if (line.length() == 0)
+				continue;
+
+			String tokens[] = line.split("[\t ]+");
+
+			if (line.startsWith("#")) {
+				continue;
+			} else if (tokens[0].equals(OBJ_VERTEX)) {
+				Point3D p = new Point3D(Float.parseFloat(tokens[1]), Float.parseFloat(tokens[2]), Float.parseFloat(tokens[3]));
+				points.add(p);
+				updateCoordinates(p);
+			} else if (tokens[0].equals(OBJ_VERTEX_TEXTURE)) {
+				// parse texture coordinates
+			} else if (tokens[0].equals(OBJ_FACE)) {
+				for (int i = 1; i < tokens.length; i++) {
+					faces.add(new Face());
+					faces.get(nFaces)
+							.addPoint(
+									points.get(Integer.parseInt(tokens[i]
+											.split("[/]")[0]) - 1));
+				}
+				nFaces++;
+			}
+			lineCount++;
+		}
+		updateMax();
+		bufferedReader.close();
+
+		System.out.println("FaceNumber" + nFaces);
+		System.err.println("Loaded " + lineCount + " lines");
+	}
+	
+	private void updateCoordinates(Point3D p){
+		if(p.getX() < xMin) {
+			xMin = p.getX();
+		}
+		
+		if(p.getY() < yMin) {
+			yMin = p.getY();
+		}
+		
+		if(p.getZ() < zMin) {
+			zMin = p.getZ();
+		}
+		
+		if(p.getX() > xMax) {
+			xMax = p.getX();
+		}
+		
+		if(p.getY() > yMax) {
+			yMax = p.getY();
+		}
+		
+		if(p.getZ() > zMax) {
+			zMax = p.getZ();
+		}
+	}
+	
+	private void updateMax(){
+		float x = Math.max(Math.abs(xMax), Math.max(Math.abs(yMax), Math.abs(zMax)));
+		float y = Math.max(Math.abs(xMin), Math.max(Math.abs(yMin), Math.abs(zMin)));
+		maxAbs = Math.max(x, y);
+	}
+}
